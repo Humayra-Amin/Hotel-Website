86c451101a332f07e982ca0370134d25f76a5865
20202034 <nuzhatkarima4@gmail.com>
2023-07-17 17:49:50 +0600

    karima

--- resources/views/room/layouts/app.blade.php
+++ resources/views/room/layouts/app.blade.php
@@ -21,7 +21,7 @@
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/sb-admin-2.min.css')}}">
     <link rel="stylesheet" href="{{asset('css/karima.css')}}">
 
     <link rel="stylesheet" href="{{asset('css/jaima.css')}}">
