2028f08109c13043a5cc092510417cb296c6b104
20202034 <nuzhatkarima4@gmail.com>
2023-07-19 19:30:29 +0600

    karima

--- resources/views/room/layouts/app.blade.php
+++ resources/views/room/layouts/app.blade.php
@@ -17,7 +17,7 @@
 
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
    
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" link href={{asset('rooms/vendor/fontawesome-free/css/all.min.css')}}>
     {{-- <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet"> --}}
@@ -30,9 +30,9 @@
 
     <link rel="stylesheet" href="{{asset('rooms/js/jaima.js')}}">
     <!--slicker--> 
-    <link rel="stylesheet" type="text/css" href="slick/slick.css"/>
+    {{-- <link rel="stylesheet" type="text/css" href="slick/slick.css"/>
 
-    <link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
+    <link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/> --}}
 
 </head>
 <body id="page-top">
@@ -48,26 +48,26 @@
     </div>
 
    <!--slicker-->
-    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-    <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-    <script type="text/javascript" src="slick/slick.min.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    {{-- <script type="text/javascript" src="slick/slick.min.js"></script> --}}
 
     
     <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{asset('rooms/vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('rooms/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src={{asset('rooms/vendor/jquery-easing/jquery.easing.min.js')}}></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
+    <script src="{{asset('js/sb-admin-2.min.js')}}></script>
 
     <!-- Page level plugins -->
-    <script src="vendor/chart.js/Chart.min.js"></script>
+    <script src={{asset('vendor/chart.js/Chart.min.js')}}></script>
 
     <!-- Page level custom scripts -->
-    <script src="js/demo/chart-area-demo.js"></script>
-    <script src="js/demo/chart-pie-demo.js"></script>
+    <script src="{{asset('js/demo/chart-area-demo.js')}}></script>
+    <script src="{{asset('js/demo/chart-pie-demo.js')}}></script>
   </body>
 </html>
