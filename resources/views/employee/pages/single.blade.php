5d7da5ff8c6551f6b7ab9432287c359d082322a7
fardin <fardinfarabi1998@gmail.com>
2023-07-20 22:02:21 +0600

    single

--- resources/views/employee/pages/single.blade.php
+++ resources/views/employee/pages/single.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 
 <div class="container mt-3">
-            <h2>Single Employee</h2>
+        <h1 class="form-h1">Single Employee</h1>
             
             <div class="container">
-            <img src="{{asset('employees/images/JR.jpg')}}" width="18px" height="20px">
-                <!-- <div class="topright">Top Right</div> -->
+            <img src="{{asset('employees/images/JR.jpg')}}" style="width:16%" >
+                
             </div>
   <form>
     <div class="row jumbotron box8">
@@ -27,47 +27,46 @@
         
       </div>
       <div class="col-sm-6 form-group">
-        <label for="name-e">Employee Id</label>
+        <label for="name-e" class="group-label">Employee Id</label>
         <p class="card-text">234</p>
        
       </div>
       <div class="col-sm-6 form-group">
-        <label for="address-1">Present Address</label>
-        <p class="card-text">House no-30,Flo Rida,USA</p>
+        <label for="address-1" class="group-label">Present Address</label>
+        <p class="card-text" >House no-30,Flo Rida,USA</p>
        
       </div>
         <div class="col-sm-6 form-group">
-        <label for="email">Email</label>
+        <label for="email" class="group-label">Email</label>
         <p class="card-text">JasonRoy@gmail.com</p>
         
       </div>
       <div class="col-sm-6 form-group">
-        <label for="email">Input NID</label>
+        <label for="email" class="group-label">Input NID</label>
         <p class="card-text">234567</p>
         
       </div>
       <div class="col-sm-6 form-group">
-        <label for="Country">Position</label>
+        <label for="Country" class="group-label">Position</label>
         <p class="card-text">Front Desk Receptionist</p>
         
       </div>
       <div class="col-sm-6 form-group">
-        <label for="Date">Date Of Birth</label>
+        <label for="Date" class="group-label">Date Of Birth</label>
         <p class="card-text"> 01/05/1995</p>
         
       </div>
       <div class="col-sm-6 form-group">
-        <label for="sex">Gender</label>
+        <label for="sex" class="group-label">Gender</label>
         <p class="card-text">Male</p>
         
       </div>
       <div class="col-sm-6 form-group">
-        <label for="tel">Contact No.</label>
+        <label for="tel" class="group-label">Contact No.</label>
         <p class="card-text"> +1 123-456-7890</p>
        
       </div>
 
-      
     </div>
   </form>
 </div>
