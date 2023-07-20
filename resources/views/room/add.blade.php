c8ea250fc44d1242e17eeffe47d5a6789b443c85
jaima <jaimajarnajj@gmail.com>
2023-07-20 13:25:45 +0600

    jaima

--- resources/views/room/add.blade.php
+++ resources/views/room/add.blade.php
@@ -35,89 +35,88 @@
 
 
                  <div class="container mt-3">
-                    
-                          <h2 class="text-left text-info1">Add Room</h2>
+                          <h2 class="text-left text-info1 custom-form-title">Add Room</h2>
                        
                     <form>
-                      <div class="row jumbotron box8">
-                        <div class="col-sm-12 mx-t3 mb-4">
-                          <h2 class="text-left text-info2">Room Details</h2>
-                        </div>
-                        <div class="col-sm-6 form-group">
-                          <label for="name-f">Room No.</label>
-                          <input type="number" class="form-control" name="roomno." id="name-f" placeholder="Enter Room No." required>
-                        </div>
-                        <div class="col-sm-6 form-group">
-                          <label for="name-l">Floor No.</label>
-                          <input type="number" class="form-control" name="floorno." id="name-l" placeholder="Enter Floor No." required>
-                        </div>
-                       
-                        <div class="col-sm-6 form-group">
-                          <label for="Category">Category</label>
-                          <select class="form-control custom-select browser-default">
-                            <option value="">Select your Room Category</option>
-                          <option>Normal</option>
-                          <option>Standered</option>
-                          <option >Premium</option>
-                          <option >Super Premium</option>
-                          <option >Queen</option>
-                         </select>
-                        </div>
+                      <div class="row box8">
                         
+                        <div class="custom-form-inner">
+                            <div class="col-sm-6 form-group custom-form-group">
+                              <label for="name-f">Room No.</label>
+                              <input type="number" class="form-control" name="roomno." id="name-f" placeholder="Enter Room No." required>
+                            </div>
+                            <div class="col-sm-6 form-group custom-form-group">
+                              <label for="name-l">Floor No.</label>
+                              <input type="number" class="form-control" name="floorno." id="name-l" placeholder="Enter Floor No." required>
+                            </div>
+                          
+                            <div class="col-sm-6 form-group custom-form-group">
+                              <label for="Category">Category</label>
+                              <select class="form-control custom-select browser-default">
+                                <option value="">Select your Room Category</option>
+                              <option>Normal</option>
+                              <option>Standered</option>
+                              <option >Premium</option>
+                              <option >Super Premium</option>
+                              <option >Queen</option>
+                            </select>
+                            </div>
+                            
+                            
+                            <div class="col-sm-6 form-group custom-form-group">
+                              <label for="Country">Room Type</label>
+                              <select class="form-control custom-select browser-default">
+                                <option value="">Select Room Type</option>
+                                <option>Single Room</option>
+                                <option>Double Room</option>
+                                <option >Quad Room</option>
+                                <option >Hollywood Twin Room</option>
+                                <option > Double-Double Room</option>
+                                <option >Interconnecting Room</option>
+                                <option > Adjoining Room</option>
+                                <option >Duplex Room</option>
+                              </select>
+                            </div>
+                            <div class="col-sm-6 form-group custom-form-group">
+                              <label for="size">Room Size</label>
+                              <input type="number" name="roomsize" class="form-control" id="number" placeholder="" required>
+                            </div>
                         
-                        <div class="col-sm-6 form-group">
-                          <label for="Country">Room Type</label>
-                          <select class="form-control custom-select browser-default">
-                          <option value="">Select Room Type</option>
-                        <option>Single Room</option>
-                        <option>Double Room</option>
-                        <option >Quad Room</option>
-                        <option >Hollywood Twin Room</option>
-                        <option > Double-Double Room</option>
-                        <option >Interconnecting Room</option>
-                        <option > Adjoining Room</option>
-                        <option >Duplex Room</option>
-                          </select>
-                        </div>
-                        <div class="col-sm-6 form-group">
-                          <label for="size">Room Size</label>
-                          <input type="number" name="roomsize" class="form-control" id="number" placeholder="" required>
-                        </div>
-                    
-                        <div class="col-sm-6 form-group">
-                          <label for="view">Room View</label>
-                          <input type="text" name="roomview" class="form-control" placeholder="" required>
-                        </div>
-
-                        <div class="col-sm-12 form-group">
-                            <label for="tel">Description</label>
-                            <input type="text" name="description" class="form-control" placeholder="" required>
-                        </div>
-
-                        <div class="col-sm-12 form-group">
-                            <label for="tel">Facilities</label>
-                            <input type="text" name="phone" class="form-control" placeholder="" required>
-                        </div>
-
-
-                        <div class="col-sm-12 form-group">
-                          <input type="file" id="upload-button" multiple accept="image/*" />
-                          <label for="upload-button"
-                            ><i class="fa-solid fa-upload"></i>&nbsp; Choose Or Drop Photos
-                          </label>
-                          <div id="error"></div>
-                          <div id="image-display"></div>
-                        </div>
-                        
-
-
-                        <div class="col-sm-4">
-                          <input type="checkbox" class="form-check d-inline" id="chb" required><label for="chb" class="form-check-label">&nbsp;I accept all terms and conditions.
-                          </label>
-                        </div>
-                  
-                        <div class="col-sm-12 form-group mb-0">
-                          <button class="btn btn-primary float-right">Submit</button>
+                            <div class="col-sm-6 form-group custom-form-group">
+                              <label for="view">Room View</label>
+                              <input type="text" name="roomview" class="form-control" placeholder="" required>
+                            </div>
+    
+                            <div class="col-sm-12 form-group custom-form-group">
+                                <label for="tel">Description</label>
+                                <input type="text" name="description" class="form-control" placeholder="" required>
+                            </div>
+    
+                            <div class="col-sm-12 form-group custom-form-group">
+                                <label for="tel">Facilities</label>
+                                <input type="text" name="phone" class="form-control" placeholder="" required>
+                            </div>
+    
+    
+                            <div class="col-sm-12 form-group custom-form-group">
+                              <input type="file" id="upload-button" multiple accept="image/*" />
+                              <label for="upload-button"
+                                ><i class="fa-solid fa-upload"></i>&nbsp; Choose Or Drop Photos
+                              </label>
+                              <div id="error"></div>
+                              <div id="image-display"></div>
+                            </div>
+                            
+    
+    
+                            <div class="col-sm-4">
+                              <input type="checkbox" class="form-check d-inline" id="chb" required><label for="chb" class="form-check-label">&nbsp;I accept all terms and conditions.
+                              </label>
+                            </div>
+                      
+                            <div class="col-sm-12 form-group mb-0">
+                              <button class="btn btn-primary float-right">Submit</button>
+                            </div>
                         </div>
                   
                       </div>
