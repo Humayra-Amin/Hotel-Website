f6e6640dd45e1aea41a4a2333d9df58126da2659
naima-tushin <naima39tushin@gmail.com>
2023-07-27 16:43:19 +0600

    tushin initial

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -78,4 +78,11 @@ public function update(Request $request)
 {
     return $request;
 }
+public function show($id)
+{
+    $employee=Employee::find($id);
+    return $employee;
+    
+    return view("employee.pages.single")->with('employee',  $employee);
+}
 }
