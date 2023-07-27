c60f063f4b3850c48d87fd703a94d406c3c4450b
fardin <fardinfarabi1998@gmail.com>
2023-07-27 18:59:00 +0600

    single

--- resources/views/employee/pages/single.blade.php
+++ resources/views/employee/pages/single.blade.php
@@ -9,29 +9,27 @@
 <div class="col-md-6 mt-4 mb-2">
 <div class="slider">
 
-    <div>
-    <img src="{{asset('employees/images/JR.jpg')}}" class="d-block w-50" >  
-    </div>
-</div>
+@foreach ($employee->image as $img)
+                    <img src='{{Storage::disk("public")->url($img)}}' class="d-block w-50">
+                @endforeach
 
+</div>
 </div>
 
 <div class="col-md-6 group-fonts">
 <div class="employee-details mt-4 mb-2">
 
 <h2 class="group-h2">Jason Roy</h2>
-                <p>Employee Id: 234</p>
-                <p>Position: Manager</p>
-                <p>email:Jasonroy10@gmail.com</p>
-                <p>Present Address: House no: 10</p>
-                <p>Gender: Male</p>
-                <p>Joining Date: 12/02/2023</p>
-                <p>Phone: +123 456 7890</p>
+                <p>Employee Id: {{$employee->employeeid}}</p>
+                <p>Position: {{$employee->position}}</p>
+                <p>email:{{$employee->email}}</p>
+                <p>Present Address: {{$employee->presentaddress}}</p>
+                <p>Gender: {{$employee->gender}}</p>
+                <p>Joining Date: {{$employee->joiningdate}}</p>
+                <p>Phone: {{$employee->phone}}</p>
 </div>
 </div>
 </div>
-
-
 
 @endsection
 
