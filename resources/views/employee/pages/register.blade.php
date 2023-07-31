a0e8209bd20b0947777dc3e782c8dba1d0267ffb
fardin <fardinfarabi1998@gmail.com>
2023-07-31 15:45:27 +0600

    single

---
+++ resources/views/employee/pages/register.blade.php
@@ -0,0 +1,42 @@
+<!-- <!DOCTYPE html>
+<head>
+   
+    <title>Register</title>
+    <link rel="stylesheet" href="Register.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script> -->
+
+
+            <div class="container">
+               <div class="row justify-content-center">
+               <div class="col-md-5">
+               <div class="card">
+                  <h2 class="card-title text-center">Register </h2>
+               <div class="card-body">
+                     <form>
+                        <div class="form-group">
+                           <input type="text" class="form-control" id="name" placeholder="Name">
+                        </div>
+                        <div class="form-group">
+                           <input type="email" class="form-control" id="email" placeholder="Email">
+                        </div>
+                                          
+                            
+                      <div class="form-group">
+                          <input type="password" class="form-control" id="password" placeholder="Password">
+                     </div>
+                      <div class="form-group">
+                          <input type="password" class="form-control" id="confirm-password" placeholder="Confirm Password">
+                     </div>
+                      <div class="d-flex flex-row align-items-center justify-content-between">
+                  
+                                 <button class="btn btn-primary">Create Account</button>                              
+                        <div class="text-center">
+                           <a class="small" href="login.html">Login</a>
+                        </div>
+
+                        </div>
+                     </form>
+
+               </head>
+            </html>
