c41d6005c5186f7b3cf94d97e661dcc1a21daf4c
20202034 <nuzhatkarima4@gmail.com>
2023-07-31 19:18:08 +0600

    karima

--- resources/views/room/edit.blade.php
+++ resources/views/room/edit.blade.php
@@ -154,20 +154,18 @@
                   
                            
                 
-                              <div class="col-sm-12">
-                                <div class="upload__box">
-                                  <div class="upload__btn-box">
-                                    <label class="upload__btn">
-                                    
-                                      Upload Room Images
-                                    
-                                      <input type="file" multiple="" data-max_length="20" class="upload__inputfile" >
-                          
-                                    </label>
-                                  </div>
-                                  <div class="upload__img-wrap"></div>
-                                </div>
-                              </div>
+                          <div class="col-sm-12">
+                           <div class="upload__box">
+                            <div class="upload__btn-box">
+                                <label class="upload__btn">
+                                   Upload Room Images
+                                  
+                                 <input  name="image[]" type="file" multiple data-max_length="20" class="upload__inputfile">
+                                </label>
+                            </div>
+                            <div class="upload__img-wrap"></div>
+                           </div>
+                          </div>
                          
                         
                             <div class="col-sm-4">
