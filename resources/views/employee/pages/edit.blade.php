5b74d15131a19dcaf7c0d91048d627ecd62cf5ec
fardin <fardinfarabi1998@gmail.com>
2023-07-31 19:19:30 +0600

    single

--- resources/views/employee/pages/edit.blade.php
+++ resources/views/employee/pages/edit.blade.php
@@ -6,7 +6,7 @@
 <h2 class="form-h2">Edit Employee</h2>
 <form action="/employee/{{$employee->id}}" method="PUT" enctype="multipart/form-data">
     @csrf
-    @method('PUT')
+    @method ('PUT')
   <div class="row jumbotron form-box8">
       
       
@@ -16,6 +16,9 @@
 <div class="col-md-6 form-group form-input-file" >
 <div class="d-flex align-items-center justify-content-start">
         <img id="file_upload" src="http://placehold.it/100" alt="your image" class="form-upload-img" />
+        @foreach ($employee->image as $image)
+                    <img src='{{Storage::disk("public")->url($image)}}' class="image">     
+                @endforeach
           <div class="form-input-file-upload">
             <span class="form-upload-label">Upload Image</span>
             <input type='file' name="image" onchange="readURL(this);" />
@@ -52,14 +55,13 @@
       <div class="col-sm-6 form-group">
         <label for="position" class="form-label">Position</label>
         <select class="form-control custom-select browser-default" name="position">
-        <option >Select your Position</option>
       <option @if($employee->position == 'Manager') selected @endif>Manager</option>
       <option @if($employee->position == 'Assistant Manager') selected @endif>Assistant Manager</option>
-      <option >Accountent</option>
-      <option >Receptionist</option>
-      <option >Security</option>
-      <option >Room Service</option>
-      <option >Janitor</option>
+      <option @if($employee->position == 'Accountent') selected @endif>Accountent</option>
+      <option @if($employee->position == 'Receptionist') selected @endif>Receptionist</option>
+      <option @if($employee->position == 'Security') selected @endif>Security</option>
+      <option @if($employee->position == 'Room Service') selected @endif>Room Service</option>
+      <option @if($employee->position == 'Janitor') selected @endif>Janitor</option>
         </select>
       </div>
       <div class="col-sm-6 form-group">
@@ -69,10 +71,9 @@
       <div class="col-sm-6 form-group">
         <label for="sex" class="form-label">Gender</label>
         <select id="sex" class="form-control browser-default custom-select" name="sex">
-        <option >Select Gender</option>
-          <option value="male">Male</option>
-          <option value="female">Female</option>
-          <option value="other">Others</option>
+          <option  @if($employee->sex == 'Male') selected @endif>Male</option>
+          <option  @if($employee->sex == 'Female') selected @endif>Female</option>
+          <option  @if($employee->sex == 'Others') selected @endif>Others</option>
         </select>
       </div>
       <div class="col-sm-6 form-group">
