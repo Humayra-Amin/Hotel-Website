431653834b6e9068f37cc1f49a87f8096689ca87
humayra amin <humaema4576@gmail.com>
2023-08-03 16:25:18 +0600

    humayra initial

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,10 +9,10 @@ class AuthController extends Controller
     //
     public function login()
     {
-        return view("");
+        return view("employee.login");
     }
     public function register()
     {
-        return view("");
+        return view("employee.register");
     }
 }
