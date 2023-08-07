730900948964d03ec0da90635256e1459fa87dc6
humayra amin <humaema4576@gmail.com>
2023-08-07 10:58:16 +0600

    humayra initial

--- app/Http/Controllers/EmployeeAuthController.php
+++ app/Http/Controllers/EmployeeAuthController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Contracts\Session\Session;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class EmployeeAuthController extends Controller
 {
@@ -15,6 +19,53 @@ class EmployeeAuthController extends Controller
     {
         return view("employee.auth.register");
     }
+
+    public function postLogin(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+    ]);
    
+    $credentials = $request->only('email', 'password');
+    if (Auth::attempt($credentials)) {
+        return redirect()->intended('dashboard')
+                    ->withSuccess('You have Successfully loggedin');
+    }
+   
+    return redirect("login")->withSuccess('Oppes! You have entered invalid credentials');
+}
+
+public function postRegistration(Request $request)
+{  
+    $request->validate([
+        'name' => 'required',
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
+       
+    $data = $request->all();
+    $check = $this->create($data);
+     
+    return redirect("dashboard")->withSuccess('Great! You have Successfully loggedin');
+}
+
+public function create(array $data)
+{
+  return User::create([
+    'name' => $data['name'],
+    'email' => $data['email'],
+    'password' => Hash::make($data['password'])
+  ]);
+}
+
+
+// public function logout() {
+//     Session::flush();
+//     Auth::logout();
+
+//     return Redirect('login');
+// }
+
 }
 
--- routes/web.php
+++ routes/web.php
@@ -38,13 +38,18 @@ Route::get('/room/edit', [RoomController::class, "edit"]);
 Route::get('/room/single', [RoomController::class, "single"]);
 
 Route::get('/employee/', [EmployeeController::class, "index"]);
-Route::get('/employee/login', [EmployeeAuthController::class, "login"]);
-Route::get('/employee/register', [EmployeeAuthController::class, "register"]);
 Route::get('/employee/add', [EmployeeController::class, "add"]);
 Route::get('/employee/single', [EmployeeController::class, "single"]);
 Route::get('/employee/{id}', [EmployeeController::class, "show"]);
 Route::get('/employee/{id}/edit', [EmployeeController::class, "edit"]);
 Route::post('/employee', [EmployeeController::class, "store"]);
 Route::put('/employee/{id}', [EmployeeController::class, "update"]);
+
+Route::get('/employee/login', [EmployeeAuthController::class, "login"]);
+Route::post('/employee/post-login', [EmployeeAuthController::class, "postLogin"]);
+Route::get('/employee/register', [EmployeeAuthController::class, "register"]);
+Route::post('/employee/post-register', [EmployeeAuthController::class, "postRegister"]);
+Route::get('/employee/logout', [EmployeeAuthController::class, "logout"]);
+
 Route::get('/drag-and-drop', 'DragAndDropController@index')->name('drag-and-drop');
 Route::post('/drag-and-drop/update', 'DragAndDropController@update')->name('drag-and-drop.update');
