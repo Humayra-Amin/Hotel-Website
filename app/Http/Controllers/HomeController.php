e4f22bc6bb44120099c29523ab9ceaafe6524c83
Humayra-Amin <humaema4576@gmail.com>
2023-08-24 17:47:21 +0600

    humayra initial

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,10 +28,23 @@ class HomeController extends Controller
     public function roombook(Request $request, $id)
     { 
        $room=Room::where("id",$id)->firstOrfail();
-        
-      
 
-   
+       $request->validate([
+        'room_id' => 'required',
+        'book' => 'required',
+    ]);
+
+        $roombook = RoomBook::find($id);
+        $roombook->room_id = $id;
+        $roombook->book = 1;
+        $roombook->save();
+
+        $room->availablerooms = $$room->availablerooms - 1;
+        $room->update();
+        
+        // if (Room::table('singleroom')->where('room_id', $request->room_id)->exists()) {
+        //     return to_route('room')->with('error', 'Sorry! The room is already booked!');
+        // }
        return view("room.singleroom")->with('room',  $room);
 
     }
