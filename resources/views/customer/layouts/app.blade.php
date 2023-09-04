1173872367f69df957dbb8a9a8c41ad96385fddc
Humayra-Amin <humaema4576@gmail.com>
2023-09-04 16:32:11 +0600

    humayra initial

--- resources/views/customer/layouts/app.blade.php
+++ resources/views/customer/layouts/app.blade.php
@@ -11,7 +11,6 @@
     <link rel="stylesheet" href="{{asset('customer/css/personalInfo.css')}}">
     <link rel="stylesheet" href="{{asset('customer/css/bookinglist.css')}}">
     <link rel="stylesheet" href="{{asset('customer/css/password.css')}}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 
 
 </head>
