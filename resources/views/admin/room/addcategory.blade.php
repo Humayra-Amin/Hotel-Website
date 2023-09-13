a4f5868d93a6f51149fa210a470e82be27a22c8c
Humayra-Amin <humaema4576@gmail.com>
2023-09-13 18:59:49 +0600

    humayra initial

---
+++ resources/views/admin/room/addcategory.blade.php
@@ -0,0 +1,123 @@
+ 
+@extends('admin.layouts.app')
+@section('app')
+<div id="content-wrapper" class="d-flex flex-column">
+
+
+  @include('admin.inc.message')
+
+
+    <!-- Main Content -->
+    <div id="content">
+
+        <!-- Topbar -->
+        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+            <!-- Sidebar Toggle (Topbar) -->
+            <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                <i class="fa fa-bars"></i>
+            </button>
+
+            <!-- Topbar Search -->
+            <form
+
+                class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+                <div class="input-group">
+                    <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
+                        aria-label="Search" aria-describedby="basic-addon2">
+                    <div class="input-group-append">
+                        <button class="btn btn-primary" type="button">
+                            <i class="fas fa-search fa-sm"></i>
+                        </button>
+                    </div>
+                </div>
+            </form>
+
+            <!-- Topbar Navbar -->
+             <!-- Nav Item - User Information -->
+             <li class="nav-item dropdown no-arrow">
+              <a  class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <span class="mr-2 d-none d-lg-inline text-gray-600 small">Admin</span>
+                  <img class="img-profile rounded-circle"
+                      src="{{asset('image/undraw_profile.svg')}}">
+              </a>
+              <!-- Dropdown - User Information -->
+              <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                  aria-labelledby="userDropdown">
+
+                  @auth
+                      <a class="dropdown-item" href="#">
+                          <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                          Profile
+                      </a>
+                      <a class="dropdown-item" href="#">
+                          <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                          Settings
+                      </a>
+                      
+                      <div class="dropdown-divider"></div>
+                      <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                          <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                          Logout
+                      </a>
+                  @else
+                      <a class="dropdown-item" href="admin/login">
+                          <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                          Login
+                      </a>
+                      <a class="dropdown-item" href="admin/login">
+                          <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                          Login
+                      </a>
+                  @endauth
+
+                  
+              </div>
+          </li>   
+       </nav>
+
+
+
+<div class="bodyfiles">
+    <form>
+      <h2 class="text-center">Add Room Category</h2>
+      <div class=" formfiles">
+        <div class=" groupform">
+            <input type="Category" class="form-control" name="Category" id="Category" placeholder="Enter Room Category" required>
+        </div>
+
+        <button type="submit" class="btn btn-primary btn-files">Submit</button>
+
+    
+    
+</div>
+</form>
+
+</div>
+
+
+{{-- <form class="row row-cols-lg-auto g-1">
+	<div class="col-1">
+		<label class="visually-hidden" for="inline-form-category">Category</label> <select
+			class="form-control" id="inline-form-category">
+			<option value="">Category</option>
+			<option value="Toys">Toys</option>
+			<option value="Books">Books</option>
+			<option value="Stationaries">Stationaries</option>
+			</select>
+	</div>
+
+	<div class="col-4">
+		<label class="visually-hidden" for="inline-form-email">Email</label>
+		<input type="text" class="form-control" id="inline-form-email"
+			placeholder="Enter email address">
+	</div>
+
+	<div class="col">
+		<button type="submit" class="btn btn-primary">Submit</button>
+	</div>
+</form>            --}}
+
+
+@endsection
