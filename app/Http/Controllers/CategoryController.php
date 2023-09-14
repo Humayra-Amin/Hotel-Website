e20aeb65d0efac1d088767cecca72e9a5cd74a68
Humayra-Amin <humaema4576@gmail.com>
2023-09-14 19:21:46 +0600

    humayra initial

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -10,37 +10,63 @@ use Illuminate\Http\Request;
 class CategoryController extends Controller
 {
     //
-    public function viewcategory()
+    public function show($id)
     {
-        return view("admin.category.viewcategory");
+        $category=Category::where("id",$id)->firstOrfail();
+        return view("admin.category.viewcategory")->with('category',  $category);        
     }
-    public function categorylist()
+    public function index()
     {
         $categories = Category::all();
         return view("admin.category.categorylist")->with('categories',  $categories);
     
     }
-    public function addcategory()
+    public function create()
     {
         return view("admin.category.addcategory");
 
+    }
+    public function update(Request $request,$id)
+    {   
+        $request->validate([
+            'categoryname' => 'required|max:50'
+,
+        ]);
+
+
+        $category = Category::find($id);
+        $category->categoryname = $request->categoryname;
+        $category->update();
+        
+
+        return redirect("admin/category")->with("success", "Category updated.");
+
+    }
+    
+    public function edit($id)
+    {
+        $category=Category::where("id",$id)->firstOrfail();
+        return view("admin.category.editcategory")->with('category',  $category);
     }
 
     public function store(Request $request)
     {
         $request->validate([
-            // 'categoryid' => 'required',
             'categoryname' => 'required|max:50',
         ]);
          
     
         $category = new Category();
-        // $category->categoryid = $request->categoryid;
         $category->categoryname = $request->categoryname;
     
         $category->save();
     
     
-        return redirect("admin/category/categorylist")->with("success", "Category added.");
+        return redirect("admin/category")->with("success", "Category added.");
+    }
+
+    public function destroy(string $id)
+    {
+        //
     }
 }
