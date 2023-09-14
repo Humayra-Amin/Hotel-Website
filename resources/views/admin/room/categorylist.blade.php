1c7a2508536cef9bf421485265748d876a88ec62
20202034 <nuzhatkarima4@gmail.com>
2023-09-14 17:25:38 +0600

    karima

--- resources/views/admin/room/categorylist.blade.php
+++ resources/views/admin/room/categorylist.blade.php
@@ -16,68 +16,56 @@
                  <!-- Topbar Search -->
                 <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
                   <div class="input-group">
-                     <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
-                                    aria-label="Search" aria-describedby="basic-addon2">
+                     <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
                      <div class="input-group-append">
-                                 <button class="btn btn-primary" type="button">
-                                        <i class="fas fa-search fa-sm"></i>
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
+                        <button class="btn btn-primary" type="button">
+                             <i class="fas fa-search fa-sm"></i>
+                         </button>
+                    </div>
+                  </div>
+                </form>
+                   <!-- Topbar Navbar -->
+                   <!-- Nav Item - User Information -->
+                   <li class="nav-item dropdown no-arrow">
+                       <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          <span class="mr-2 d-none d-lg-inline text-gray-600 small">Admin</span>
+                          <img class="img-profile rounded-circle" src="{{asset('image/undraw_profile.svg')}}">
+                       </a>
+                         <!-- Dropdown - User Information -->
+                         <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
 
-                        <!-- Topbar Navbar -->
-                         <!-- Nav Item - User Information -->
-                         <li class="nav-item dropdown no-arrow">
-                            <a  class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Admin</span>
-                                <img class="img-profile rounded-circle"
-                                    src="{{asset('image/undraw_profile.svg')}}">
+                          @auth
+                            <a class="dropdown-item" href="#">
+                                <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                                 Profile
                             </a>
-                            <!-- Dropdown - User Information -->
-                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                                aria-labelledby="userDropdown">
-
-                                @auth
-                                    <a class="dropdown-item" href="#">
-                                        <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                        Profile
-                                    </a>
-                                    <a class="dropdown-item" href="#">
-                                        <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                                        Settings
-                                    </a>
+                            <a class="dropdown-item" href="#">
+                                 <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                                 Settings
+                            </a>
                                     
-                                    <div class="dropdown-divider"></div>
-                                    <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                                        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                                 <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                         Logout
-                                    </a>
-                                @else
-                                    <a class="dropdown-item" href="room/login">
-                                        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                        Login
-                                    </a>
-                                    <a class="dropdown-item" href="room/login">
-                                        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                        Login
-                                    </a>
-                                @endauth
-
-                                
-                            </div>
+                            </a>
+                          @else
+                            <a class="dropdown-item" href="room/login">
+                                 <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                 Login
+                            </a>
+                            <a class="dropdown-item" href="room/login">
+                                 <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                 Login
+                            </a>
+                           @endauth
+                         </div>
                         </li>   
                     </nav>
-  
-        
 
         <div class="container">
         <div class="row">
-            
-
-
-            @include('admin.inc.message')
+        @include('admin.inc.message')
 
 
             <div class="col-md-12 d-flex align-items-center justify-content-between">
