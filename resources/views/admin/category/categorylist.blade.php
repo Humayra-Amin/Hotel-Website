2e2c23ddd1004ac3b3ee2a6c26e7f9e28eefc9bb
Humayra-Amin <humaema4576@gmail.com>
2023-09-14 18:42:13 +0600

    humayra initial

--- resources/views/admin/category/categorylist.blade.php
+++ resources/views/admin/category/categorylist.blade.php
@@ -109,7 +109,7 @@
                                 <td>{{$category->categoryname}}</td>
                                 <td class="text-center">
                                  <a href="/admin/category/{{$category->id}}/edit/" title="edit" class="btn btn-outline-dark btn-pencil"><i class="fa-solid fa-pencil"></i></a>
-                                 <a href="/admin/{{$category->id}}/delete/" title="delete" class="btn btn-outline-danger btn-trash"><i class="fa-solid fa-trash"></i></a>
+                                 <a href="/admin/category/{{$category->id}}/delete/" title="delete" class="btn btn-outline-danger btn-trash"><i class="fa-solid fa-trash"></i></a>
                                  </td>
                               </tr>
                            @endforeach
