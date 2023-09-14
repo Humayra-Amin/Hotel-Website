4f2e9e1081ed97c0d0f5577eadc4647a066fdc94
20202034 <nuzhatkarima4@gmail.com>
2023-09-14 15:52:24 +0600

    karima

--- resources/views/admin/room/index.blade.php
+++ resources/views/admin/room/index.blade.php
@@ -21,7 +21,7 @@
                 
 
                         <!-- Topbar Search -->
-                        <form
+                        <form>
                             class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
                             <div class="input-group">
                                 <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
@@ -63,11 +63,11 @@
                                         Logout
                                     </a>
                                 @else
-                                    <a class="dropdown-item" href="room/login">
+                                    <a class="dropdown-item" href="admin/room/login">
                                         <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                         Login
                                     </a>
-                                    <a class="dropdown-item" href="room/login">
+                                    <a class="dropdown-item" href="admin/room/login">
                                         <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                         Login
                                     </a>
