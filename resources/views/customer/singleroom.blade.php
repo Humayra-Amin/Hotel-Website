51372d19e72e0d73ebf9888ef9f9adc3c987d5ec
20202034 <nuzhatkarima4@gmail.com>
2023-10-03 17:29:53 +0600

    karima

--- resources/views/customer/singleroom.blade.php
+++ resources/views/customer/singleroom.blade.php
@@ -2,38 +2,6 @@
 
 @section('apps')
 
-
-
-{{-- <div class="container single-context">
- 
-        <div class="col-md-6 mt-5">
-            <div id="roomCarousel" class="carousel slide" data-ride="carousel">
-                <div class="carousel-inner mt-4">
-                    @if ($room->image)
-                    @foreach ($room->image as $img)
-                    <div>
-                        <img src='{{Storage::disk("public")->url($img)}}' class="d-block w-100" >
-                    </div>
-                    @endforeach
-                    @endif
-                  
-                    <!-- Add more carousel items for additional images -->
-                </div>
-              
-             
-            </div>
-        </div>
-     
-        <div class="col-md-6">
-            <h1 class="room-text">{{$room->roomtitle}}</h1>
-            <p class="lead">{{$room->description}}</p>
-            <p class="lead">{{$room->roomtype}}</p>
-            <p class="lead">{{$room->roomsize}}</p>
-            <p class="lead">{{$room->roomview}}</p>
-      
-            <p class="lead"> {{$room->price}}</p> --}}
-
-
   <div class="room-details">
     <!-- Room Details Section -->
     <section class="my-5">
@@ -49,18 +17,7 @@
                   <img src='{{Storage::disk("public")->url($img)}}' class="d-block w-100" >
               </div>
               @endforeach
-              @endif
-
-
-              {{-- <div class="carousel-item">
-                <img src="images/deluxe room.jpg" class="d-block" alt="Image 2">
-              </div>
-              <div class="carousel-item">
-                <img src="images/family room.jpg" class="d-block" alt="Image 3">
-              </div> --}}
-
-
-              <!-- Add more images as needed -->
+              @endif  
             </div>
             <a class="carousel-control-prev" href="#roomSlider" role="button" data-slide="prev">
               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -99,30 +56,8 @@
     </section>
     </div>
 
-
-
-
-
-
-            {{-- @include('admin.inc.message') --}}
-
-            {{-- <form action="/roombook/{{$room->id}}" method="POST" enctype="multipart/form-data">
-                @csrf
-                @method('PUT') --}}
-                    {{-- <button id="bookButton" class="btn btn-primary" href="customer/bookmodal">Book Now</button> --}}
-                    {{-- <button  type="button" class="btn btn-primary" data-toggle="modal" data-target="#reservationModal">Book Now</button> --}}
-
-            {{-- </form> --}}
-        {{-- </div> --}}
-   
 </div>
-{{-- 
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<script src="script.js"></script>
-</body>
-</html> --}}
+
 
 
 @include('admin.inc.message')
@@ -137,7 +72,7 @@
 
                 <div class="form-group">
                     <label for="name">Name</label>
-                    <input type="text"  placeholder="Enter your name" name="name" class="form-control" id="name" required>
+                    <input type="text" placeholder="Enter your name" name="name" class="form-control" id="name" required>
                 </div>
                 <div class="form-group">
                     <label for="email">Email</label>
@@ -194,75 +129,5 @@
 
 @endsection
 
-{{-- start from here --}}
-
-
-
-{{-- <div class="room-details">
-  <!-- Room Details Section -->
-    <div class="row">
-      <div class="col-md-6 room-slider">
-        <!-- Slider Carousel -->
-        <div id="roomSlider" class="carousel slide  mt-5" data-ride="carousel">
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img src="images/luxary room.jpg" class="d-block" alt="Image 1">
-            </div>
-            <div class="carousel-item">
-              <img src="images/deluxe room.jpg" class="d-block" alt="Image 2">
-            </div>
-            <div class="carousel-item">
-              <img src="images/family room.jpg" class="d-block" alt="Image 3">
-            </div>
-            <!-- Add more images as needed -->
-          </div>
-          <a class="carousel-control-prev" href="#roomSlider" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a class="carousel-control-next" href="#roomSlider" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
-        </div>
-      </div>
-      
-        <!-- Room Details -->
-        <div class="col-md-6">
-          <h2>Deluxe Room</h2>
-          <p>Experience luxury and comfort in our Deluxe Room. This spacious room features modern amenities, a king-sized bed, and a breathtaking view of the city skyline.</p>
-          <div class="room-facilities">
-            <div class="row">
-            <div class="col-lg-6 mt-3">
-              <img src="images/square-fit.png" class="roomicon-image" alt=""><span class="mx-2"> 40 m² </span>
-            </div>
-          <div class="col-lg-6 mt-3">
-            <img src="images/wifi.png" class="roomicon-image" alt=""><span class="mx-2"> Inclusive WiFi </span>
-        </div>
-        <div class="col-lg-6 mt-3">
-          <img src="images/bed.png" class="roomicon-image" alt=""><span class="mx-2"> King </span>
-          <!-- <i class= "fa fa-solid fa-bed-front"></i><span> King </span> -->
-        </div>
-        <div class="col-lg-6 mt-3">
-          <img src="images/people.png" class="roomicon-image" alt=""><span class="mx-2"> Up to 3 guest </span>
-        </div>
-        <div class="col-lg-6 mt-3">
-          <img src="images/breakfast.png" class="roomicon-image" alt=""><span class="mx-2"> Complimentary Breakfast </span>
-        </div>
-        <div class="col-lg-6 mt-3">
-          <img src="images/swim.png" class="roomicon-image"  alt=""><span class="mx-2"> Complimentary Swimming</span>
-        </div>
-        <div class="col-lg-12 roomprice">
-        <p class="roompricetag mt-2">$300</p>
-        <p class="roomduration">per night</p>
-        </div>
-        <div class="col-lg-12 roombook ">
-        <button class="roombook-btn">Book Now</button>
-      </div>
-            </div>
-    </div>
-    </div>
-   </div>
-  </div> --}}
 
 
