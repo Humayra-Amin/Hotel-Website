d7dd66efdd1a5d6e0138dda972eca0dde0ced5b7
Humayra-Amin <humaema4576@gmail.com>
2023-10-03 19:18:12 +0600

    humayra initial

--- resources/views/customer/login.blade.php
+++ resources/views/customer/login.blade.php
@@ -1,31 +1,49 @@
+@extends('customer.layouts.apps')
 
 
-    @extends('customer.layouts.apps')
-    @section('apps')
+@section('apps')
+
+
+
 
  <div class="logfile">
+
+
     <div class="custom-file">
-     <section class="Form my-5 mx-6 py-5">        
+
+
+     <section class="Form my-5 mx-6 py-5">   
+        
+        
         <div class="container">
-            @include('admin.inc.message') 
+
+
+
+ @include('admin.inc.message') 
+
+
+
             <div class="row g-0 custom-card-group" >
+
+
+
                 <div class="col-lg-5">
                     <img src="{{asset('image/2.jpg')}}" class="img-fluid fileimg" alt="">
                 </div>
+
+
+
+
                 <div class="col-lg-7 px-5 pt-5 text-center py-5">
+
+
                     <h1 class="font-weight-bold py-3">Login</h1>
-                    <!-- <h4>Sign in to your account</h4> -->
+
+
+
                     <form class="login-form" action="{{ url('/post-login') }}" method="POST">
                         @csrf
 
-                        {{-- <div class="form-row mt-3">
-                            <div class="offset-1 col-lg-10">
-                                <input type="name" placeholder="Username" name="name" class="inp px-3 my-3">
-                                @if ($errors->has('name'))
-                                <span class="text-danger">{{ $errors->first('name') }}</span>
-                                @endif
-                            </div>
-                        </div> --}}
 
                         <div class="title-row d-flex flex-column ">
                             <div class="offset-1 col-lg-10">
@@ -36,6 +54,8 @@
                             </div>
                         </div>
 
+
+
                         <div class="title-row">
                             <div class="offset-1 col-lg-10">
                                 <input type="password" placeholder="Password" name="password" class="file-inp">
@@ -43,21 +63,40 @@
                                 <span class="text-danger">{{ $errors->first('password') }}</span>
                                  @endif
                             </div>
-                       
                         </div>
+
+
+
                         <div class="title-row py-3">
                             <div class="offset-1 col-lg-10">
                                 {{-- <button type="button" class="btn1 mt-2 mb-4">Login</button>                            --}}
                                 <button type="submit" class="login-btn1 mt-2 mb-4" >Login</button>
                             </div>
                         </div>
+
+
+
+                        
                         <a href="#" class="px-5 pt-10 linkbtn">Forgot Password?</a>
                         <p class="mt-0 mb-0 ">Don't have an Account? <a href="/register" class="linkbtn">Register here</a></p>
+                    
+                    
+                    
                     </form>
+
+
+
                 </div>
+
             </div>
+
         </div>
+
     </section>
+
  </div>
+
 </div>
+
+
 @endsection
--- resources/views/customer/singleroom.blade.php
+++ resources/views/customer/singleroom.blade.php
@@ -2,11 +2,19 @@
 
 @section('apps')
 
+
+
   <div class="room-details">
+
+
+
     <!-- Room Details Section -->
     <section class="my-5">
       <div class="row">
         <div class="col-md-6 room-slider">
+
+
+
           <!-- Slider Carousel -->
           <div id="roomSlider" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
@@ -16,20 +24,39 @@
               <div>
                   <img src='{{Storage::disk("public")->url($img)}}' class="d-block w-100" >
               </div>
+
+
               @endforeach
+              
+              
               @endif  
+
+
             </div>
+
+
+
             <a class="carousel-control-prev" href="#roomSlider" role="button" data-slide="prev">
               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
               <span class="sr-only">Previous</span>
             </a>
+
+
+
             <a class="carousel-control-next" href="#roomSlider" role="button" data-slide="next">
               <span class="carousel-control-next-icon" aria-hidden="true"></span>
               <span class="sr-only">Next</span>
             </a>
+
+
+
           </div>
+
         </div>
+
+
         <div class="col-md-6">
+
           <!-- Room Details -->
             <h2>{{$room->roomtitle}}</h2>
             <p>Description: {{$room->description}}</p>
@@ -46,15 +73,21 @@
             <p>{{$room->roomprice}}</p>
 
 
-            @include('admin.inc.message')
+
+@include('admin.inc.message')
+
 
 
             <button  type="button" class="btn btn-primary" data-toggle="modal" data-target="#reservationModal">Book Now</button>
 
           </div>
+
         </div>
+
     </section>
+
     </div>
+
 
 </div>
 
@@ -62,22 +95,35 @@
 
 @include('admin.inc.message')
 
+
+
 <div class="modal fade" id="reservationModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
+    
+  <div class="modal-dialog" role="document">
+
         <div class="modal-content">
+
             <div class="modal-body">
+
+
 
               <form action="{{ url('/roombook') }}" method="POST" enctype="multipart/form-data">
                 @csrf 
+
 
                 <div class="form-group">
                     <label for="name">Name</label>
                     <input type="text" placeholder="Enter your name" name="name" class="form-control" id="name" required>
                 </div>
+
+
+
                 <div class="form-group">
                     <label for="email">Email</label>
                     <input type="email"  placeholder="Enter your email" name="email" class="form-control" id="email" required>
                 </div>
+
+
 
                 <div class="form-group">
                   <label for="roomcategory" class="reserve-label">Room Category </label>
@@ -89,6 +135,8 @@
                       <option value="Queen">Queen</option>
                   </select>
                 </div>
+
+
 
                 <div class="form-group">
                   <label for="roomType">Room Type </label>
@@ -104,6 +152,8 @@
                   </select>
                 </div>
 
+
+
                 <div class="form-group">
                     <label for="checkInDate">Check-in Date</label>
                     <input type="date" class="form-control" name="checkInDate" id="checkInDate" required>
@@ -113,6 +163,8 @@
                     <input type="date" class="form-control" name="checkOutDate" id="checkOutDate" required>
                 </div>
 
+
+
               
             </div>
             <div class="modal-footer">
@@ -120,11 +172,18 @@
                 <button type="button" class="btn btn-primary" id="reserveButton">Reserve</button>
             </div>
 
+
+
           </form>
           
+
+
         </div>
+
     </div>
+
 </div>
+
 
 
 
