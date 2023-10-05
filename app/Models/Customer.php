c64b7399298cada061d13ef35501e6a3e703dec8
20202034 <nuzhatkarima4@gmail.com>
2023-10-05 17:37:52 +0600

    karima

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -23,10 +23,7 @@ class Customer extends Authenticatable
         'name',
         'email',
         'password',
-        
-        'id',
-        'roomno',            
-        'status',
+        'id',           
         'image',
 
     ];
